e32cdd4083a4c40a388e83ee00f58289923266f9
JCamyre <JWcamry03@gmail.com>
2021-06-09 15:48:35 -0700

    Added to loginsystem.cpp

--- loginsystem.cpp
+++ loginsystem.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 // iostream stands for standard input-output stream. cin, cout. 
+#include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -12,78 +14,64 @@ void signup(){
 }
 
 int main(){
-<<<<<<< HEAD
+    // string un, pw;
+    // string username = "joemama";
+    // // ifstream is the variable type, read is the name of the variable, and we passing in the location of the file as an argument
+    // // Good thing with ifstream: even if file doesn't exist, doesn't throw an error.
+    // ifstream read("data\\" + username + ".txt");
+    // getline(read, un);
+    // getline(read, pw);
+    // cout << un << endl << pw << endl;
+
     bool loggedin = false;
     int choice;
-    string username;
-    string password;
+    string username; string password; string un; string pw;
     cout << "Would you like to login or register an account:" << endl;
     cout << "Login: 1" << endl; cout << "Sign Up: 2" << endl;
     cin >> choice;
-    while(!loggedin) {
-        switch(choice) {
-            // Login
-            case 1:
-                cout << "Please enter your username and password." << endl;
-                cout << "Username: "; cin >> username;
-                cout << "Password: "; cin >> password;
-                // Logic for checking for file with "username.txt", and check to see if info the same.
-                break;
-            // Register
-            case 2: 
-                string retyped_password;
-                cout << "Please enter your desired username and password." << endl;
-                cout << "Username: "; cin >> username;
-                cout << "Password: "; cin >> password;
-                cout << "Confirm password: "; cin >> retyped_password;
-                if(password == retyped_password) {
-                    cout << "Congratulations, your account has been created!" << endl;
-                    // Condition for if username already exists. else if() {}
-                } else {
-                    cout << "There seems to be an error while creating your account. Ensure you retyped your password correctly." << endl;
-                    while (password != retyped_password) {
-                        cout << "Please enter your desired username and password." << endl;
-                        cout << "Username: "; cin >> username;
-                        cout << "Password: "; cin >> password;
-                        cout << "Confirm password: "; cin >> retyped_password;
-                    }
+
+    switch(choice) {
+        // Login
+        case 1:
+            cout << "Please enter your username and password." << endl;
+            cout << "Username: "; cin >> username;
+            cout << "Password: "; cin >> password;
+
+            ifstream read("data\\" + username + ".txt");
+            getline(read, un);
+            getline(read, pw);
+            if(username==un && password==pw){
+                cout << "You are logged in!" << endl;
+            } else {
+                while (!loggedin){
+
                 }
-                // Add code for registering user
-        }
+                cout << "Sorry, incorrect password or username, try again!" << endl;
+            }
+            // Logic for checking for file with "username.txt", and check to see if info the same.
+            break;
+        // Register
+        case 2: 
+            string retyped_password;
+            cout << "Please enter your desired username and password." << endl;
+            cout << "Username: "; cin >> username;
+            cout << "Password: "; cin >> password;
+            cout << "Confirm password: "; cin >> retyped_password;
+            if(password == retyped_password) {
+                cout << "Congratulations, your account has been created!" << endl;
+                // Condition for if username already exists. else if() {}
+            } else {
+                cout << "There seems to be an error while creating your account. Ensure you retyped your password correctly." << endl;
+                while (!loggedin) {
+                    cout << "Please enter your desired username and password." << endl;
+                    cout << "Username: "; cin >> username;
+                    cout << "Password: "; cin >> password;
+                    cout << "Confirm password: "; cin >> retyped_password;
+                }
+            }
+            // Add code for registering user
     }
-=======
-    // bool loggedin = false;
-    // int choice;
-    // string username;
-    // string password;
-    // cout << "Would you like to login or register an account:" << endl;
-    // cout << "Login: 1" << endl; cout << "Sign Up: 2" << endl;
-    // cin >> choice;
-    // while(!loggedin) {
-    //     switch(choice) {
-    //         // Login
-    //         case 1:
-    //             cout << "Please enter your username and password." << endl;
-    //             cout << "Username: "; cin >> username;
-    //             cout << "Password: "; cin >> password;
-    //             break;
-    //         // Register
-    //         case 2: 
-    //             string retyped_password;
-    //             cout << "Please enter your desired username and password." << endl;
-    //             cout << "Username: "; cin >> username;
-    //             cout << "Password: "; cin >> password;
-    //             cout << "Confirm password: "; cin >> retyped_password;
-    //             if(password == retyped_password) {
-    //                 cout << "Congratulations, your account has been created!" << endl;
-    //                 // Add code for registering user
-    //                 // Condition for if username already exists. else if() {}
-    //             } else {
-    //                 cout << "There seems to be an error while creating your account. Ensure you retyped your password correctly." << endl;
-    //             }
-    //     }
-    // }
->>>>>>> ecce41064e9fafe1433f381f34187ae22868c874
+
 
     return 0;
 }
