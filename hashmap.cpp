a53c007301110f788de970c0d3589cf756037ca2
JCamyre <JWcamry03@gmail.com>
2021-06-09 13:31:23 -0700

    Added loginsystem and hashmap files to updated git repository.

---
+++ hashmap.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    // Ohhhh, it's an attribute
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+};
+
+int main() {
+    // std::string would refer to c++ standard library, and the string module from it.
+    vector<int>v{1, 2, 3};
+    cout << sizeof("") << endl;
+    cout << v.size() << endl;
+
+    return 0;
+}
---
+++ loginsystem.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+// iostream stands for standard input-output stream. cin, cout. 
+
+using namespace std;
+
+void login(){
+
+}
+
+void signup(){
+
+}
+
+int main(){
+    bool loggedin = false;
+    int choice;
+    string username;
+    string password;
+    cout << "Would you like to login or register an account:" << endl;
+    cout << "Login: 1" << endl; cout << "Sign Up: 2" << endl;
+    cin >> choice;
+    while(!loggedin) {
+        switch(choice) {
+            // Login
+            case 1:
+                cout << "Please enter your username and password." << endl;
+                cout << "Username: "; cin >> username;
+                cout << "Password: "; cin >> password;
+                // Logic for checking for file with "username.txt", and check to see if info the same.
+                break;
+            // Register
+            case 2: 
+                string retyped_password;
+                cout << "Please enter your desired username and password." << endl;
+                cout << "Username: "; cin >> username;
+                cout << "Password: "; cin >> password;
+                cout << "Confirm password: "; cin >> retyped_password;
+                if(password == retyped_password) {
+                    cout << "Congratulations, your account has been created!" << endl;
+                    // Condition for if username already exists. else if() {}
+                } else {
+                    cout << "There seems to be an error while creating your account. Ensure you retyped your password correctly." << endl;
+                    while (password != retyped_password) {
+                        cout << "Please enter your desired username and password." << endl;
+                        cout << "Username: "; cin >> username;
+                        cout << "Password: "; cin >> password;
+                        cout << "Confirm password: "; cin >> retyped_password;
+                    }
+                }
+                // Add code for registering user
+        }
+    }
+
+    return 0;
+}
