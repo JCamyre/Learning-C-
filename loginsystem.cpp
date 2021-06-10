b309765a1f889547656bdb577c57f92cd04a1f77
JCamyre <JWcamry03@gmail.com>
2021-06-09 18:13:40 -0700

    Cleaned up code and did functional decomposition.

--- loginsystem.cpp
+++ loginsystem.cpp
@@ -5,25 +5,55 @@
 
 using namespace std;
 
-void login(){
+void login(string username, string password, string un, string pw){
+    cout << "Please enter your username and password." << endl;
+    cout << "Username: "; cin >> username;
+    cout << "Password: "; cin >> password;
 
+    ifstream read("data\\" + username + ".txt");
+    getline(read, un);
+    getline(read, pw);
+    if(username==un && password==pw){
+        cout << "Hello, " << username << " you are logged in!" << endl; 
+    } else {
+        while (true){
+            cout << "Sorry, incorrect password or username, try again!" << endl;
+            cout << "Username: "; cin >> username;
+            cout << "Password: "; cin >> password;
+            if(username==un && password==pw){
+                cout << "Hello, " << username << " you are logged in!" << endl; 
+                break;
+            }
+        }
+    }
+    system("PAUSE");
 }
 
-void signup(){
-
+void signup(string username, string password){
+    string retyped_password;
+    cout << "Please enter your desired username and password." << endl;
+    cout << "Username: "; cin >> username;
+    cout << "Password: "; cin >> password;
+    cout << "Confirm password: "; cin >> retyped_password;
+    if(password == retyped_password) {
+        cout << "Congratulations, your account has been created!" << endl;
+        // Condition for if username already exists. else if() {}
+    } else {
+        cout << "There seems to be an error while creating your account. Ensure you retyped your password correctly." << endl;
+        while (true) {
+            cout << "Please enter your desired username and password." << endl;
+            cout << "Username: "; cin >> username;
+            cout << "Password: "; cin >> password;
+            cout << "Confirm password: "; cin >> retyped_password;
+        }
+    }
+    ofstream file;
+    file.open("data\\" + username + ".txt");
+    file << username << endl << password;
+    file.close();
 }
 
 int main(){
-    // string un, pw;
-    // string username = "joemama";
-    // // ifstream is the variable type, read is the name of the variable, and we passing in the location of the file as an argument
-    // // Good thing with ifstream: even if file doesn't exist, doesn't throw an error.
-    // ifstream read("data\\" + username + ".txt");
-    // getline(read, un);
-    // getline(read, pw);
-    // cout << un << endl << pw << endl;
-
-    bool loggedin = false;
     int choice;
     string username; string password; string un; string pw;
     cout << "Would you like to login or register an account:" << endl;
@@ -33,50 +63,10 @@ int main(){
         cout << "Login: 1" << endl; cout << "Sign Up: 2" << endl;
         cin >> choice;
         if (choice == 1) {
-            cout << "Please enter your username and password." << endl;
-            cout << "Username: "; cin >> username;
-            cout << "Password: "; cin >> password;
-
-            ifstream read("data\\" + username + ".txt");
-            getline(read, un);
-            getline(read, pw);
-            if(username==un && password==pw){
-                cout << "Hello, " << username << " you are logged in!" << endl; 
-            } else {
-                while (!loggedin){
-                    cout << "Sorry, incorrect password or username, try again!" << endl;
-                    cout << "Username: "; cin >> username;
-                    cout << "Password: "; cin >> password;
-                    if(username==un && password==pw){
-                        cout << "Hello, " << username << " you are logged in!" << endl; 
-                        break;
-                    }
-                }
-            }
-            system("PAUSE");
-            break;
+            login(username, password, un, pw);
+            break;         
         } else if (choice == 2){
-            string retyped_password;
-            cout << "Please enter your desired username and password." << endl;
-            cout << "Username: "; cin >> username;
-            cout << "Password: "; cin >> password;
-            cout << "Confirm password: "; cin >> retyped_password;
-            if(password == retyped_password) {
-                cout << "Congratulations, your account has been created!" << endl;
-                // Condition for if username already exists. else if() {}
-            } else {
-                cout << "There seems to be an error while creating your account. Ensure you retyped your password correctly." << endl;
-                while (!loggedin) {
-                    cout << "Please enter your desired username and password." << endl;
-                    cout << "Username: "; cin >> username;
-                    cout << "Password: "; cin >> password;
-                    cout << "Confirm password: "; cin >> retyped_password;
-                }
-            }
-            ofstream file;
-            file.open("data\\" + username + ".txt");
-            file << username << endl << password;
-            file.close();
+            signup(username, password);
         }
     }
 
