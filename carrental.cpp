5547d9d77fab8ef24f860522a33c0fb9082ec132
JCamyre <JWcamry03@gmail.com>
2021-06-10 18:18:07 -0700

    Added logic for searching for strings in lines of a file

--- carrental.cpp
+++ carrental.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <ctime>
+#include <string>
 
 using namespace std;
 // Maybe track year of each car
@@ -10,14 +11,39 @@ void log_rental(string name, string phonenumber, string licenseplate, string mak
     char* dt = ctime(&now);
     // Don't have to do std::ofstream, but looks cool.
     std::ofstream file;
+    // std::ios_base::app is how we append to file instead of overwrite.
     file.open("carrental\\log.txt", std::ios_base::app);
-    file << name << endl << phonenumber << endl << licenseplate << endl << make << endl << model << endl << payment << endl << days << endl << dt << "==========================================================" << endl;
+    file << "Name: " << name << endl << "Phone number: " << phonenumber << endl << "License plate: " << licenseplate << endl << "Make: " << make << endl;
+    file << "Model: " << model << endl << "Payment: " << payment << endl << "Days: " << days << endl << "Date: " << dt << "==========================================================" << endl;
     file.close();
+    // If a car is rented, we have to take off available.txt. Identify car by License plate.
+    string line;
+    ifstream read("carrental\\available.txt");
+    while(getline(read, line)){
+        cout << line.find("Lexus") << endl;
+    }
 }
 
+void log_available_cars(){
+    
+}
 
 int main(){
-    log_rental("Joseph", "9167376676", "8AO4DWOS", "Lexus", "GS350", 252.50, 7);
+    int choice;
+
+    if(choice==1){
+        // Customer wants to rent a car. If car available, log_rental();
+    }
+    // log_rental("Joseph", "9167376676", "8AO4DWOS", "Lexus", "GS350", 252.50, 7);
+    string line;
+    ifstream read("carrental\\log.txt");
+    while(getline(read, line)){
+        int a = line.string::find("Lexus");
+        // std::string::npos is equal to the last possible position for the string, which is what is returned if line.string::find() doesn't get a match
+        if(a!=std::string::npos){
+            cout << line << endl;
+        }
+    }
 
     return 0;
 }
