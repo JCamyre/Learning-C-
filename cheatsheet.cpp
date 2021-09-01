0f83152f75a4d953d766754cdcfee5b3dfbf74de
Joseph Camyre <jwcamry03@gmail.com>
2021-09-01 09:20:07 -0700

    add cheatsheet

---
+++ cheatsheet.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+// I should be more explicit with the methods I am using. I.e: std::cout << std::endl;, std::printf('\n');
+
+void data_types(){
+    
+}
+
+// Classes
+
+void pointers(){
+    // dereferencing
+    // call by reference vs call by value
+}
+
+void file_io(){
+
+}
+
+int main() {
+    data_types();
+
+    return 0;
+}
